8ac5a729b315f642784b056efe35a50ffb317608
ck <chenke@deepin.org>
2023-12-14 16:07:04 +0800

    fix(build): install translations failed
    update translations
    Warnning: Do Not delete qm files!

--- translations/dde-widgets-notification_zh_CN.ts
+++ translations/dde-widgets-notification_zh_CN.ts
@@ -4,17 +4,17 @@
 <context>
     <name>BubbleBase</name>
     <message>
-        <location filename="../notification/bubbleitem.cpp" line="446"/>
+        <location filename="../notification/bubbleitem.cpp" line="439"/>
         <source>Unpin</source>
         <translation>取消置顶</translation>
     </message>
     <message>
-        <location filename="../notification/bubbleitem.cpp" line="446"/>
+        <location filename="../notification/bubbleitem.cpp" line="439"/>
         <source>Pin</source>
         <translation>置顶</translation>
     </message>
     <message>
-        <location filename="../notification/bubbleitem.cpp" line="453"/>
+        <location filename="../notification/bubbleitem.cpp" line="446"/>
         <source>Notification settings</source>
         <translation>通知设置</translation>
     </message>
@@ -22,22 +22,22 @@
 <context>
     <name>BubbleItem</name>
     <message>
-        <location filename="../notification/bubbleitem.cpp" line="242"/>
+        <location filename="../notification/bubbleitem.cpp" line="233"/>
         <source>Just now</source>
         <translation>刚刚</translation>
     </message>
     <message>
-        <location filename="../notification/bubbleitem.cpp" line="244"/>
+        <location filename="../notification/bubbleitem.cpp" line="235"/>
         <source>%1 minutes ago</source>
         <translation>%1 分钟前</translation>
     </message>
     <message>
-        <location filename="../notification/bubbleitem.cpp" line="246"/>
+        <location filename="../notification/bubbleitem.cpp" line="237"/>
         <source>%1 hours ago</source>
         <translation>%1 小时前</translation>
     </message>
     <message>
-        <location filename="../notification/bubbleitem.cpp" line="249"/>
+        <location filename="../notification/bubbleitem.cpp" line="240"/>
         <source>Yesterday </source>
         <translation>昨天 </translation>
     </message>
@@ -45,12 +45,12 @@
 <context>
     <name>NotificationWidgetPlugin</name>
     <message>
-        <location filename="../notification/plugin.cpp" line="30"/>
+        <location filename="../notification/plugin.cpp" line="13"/>
         <source>Notification</source>
         <translation>通知</translation>
     </message>
     <message>
-        <location filename="../notification/plugin.cpp" line="35"/>
+        <location filename="../notification/plugin.cpp" line="18"/>
         <source>Notification Center</source>
         <translation>通知中心</translation>
     </message>
@@ -58,22 +58,22 @@
 <context>
     <name>NotifyCenterWidget</name>
     <message>
-        <location filename="../notification/notifycenterwidget.cpp" line="78"/>
+        <location filename="../notification/notifycenterwidget.cpp" line="59"/>
         <source>Notification Center</source>
         <translation>通知中心</translation>
     </message>
     <message>
-        <location filename="../notification/notifycenterwidget.cpp" line="165"/>
+        <location filename="../notification/notifycenterwidget.cpp" line="102"/>
         <source>No new notifications</source>
         <translation>没有最近的通知消息</translation>
     </message>
     <message>
-        <location filename="../notification/notifycenterwidget.cpp" line="168"/>
+        <location filename="../notification/notifycenterwidget.cpp" line="172"/>
         <source>%1 more notifications</source>
         <translation>剩余%1条通知</translation>
     </message>
     <message>
-        <location filename="../notification/notifycenterwidget.cpp" line="210"/>
+        <location filename="../notification/notifycenterwidget.cpp" line="257"/>
         <source>Notification settings</source>
         <translation>通知设置</translation>
     </message>
@@ -81,7 +81,7 @@
 <context>
     <name>NotifyWidget</name>
     <message>
-        <location filename="../notification/notifywidget.cpp" line="40"/>
+        <location filename="../notification/notifywidget.cpp" line="23"/>
         <source>No system notifications</source>
         <translation>没有系统通知</translation>
     </message>
